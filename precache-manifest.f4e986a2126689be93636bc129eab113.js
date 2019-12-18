7cd545c409bf152e61a34ea8f1d21f6c22b8369e
Ze Feng Huang <zefengh@apadmi.com>
2019-12-18 14:44:27 +0000

    Updates

---
+++ precache-manifest.f4e986a2126689be93636bc129eab113.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fa75d19e17b598d3210f6bf6242fc9e",
+    "url": "/WBSite/index.html"
+  },
+  {
+    "revision": "132f8aafe7bd1629ecfc",
+    "url": "/WBSite/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "151e7e4304d438e17619",
+    "url": "/WBSite/static/js/2.dd69e89b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/WBSite/static/js/2.dd69e89b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "132f8aafe7bd1629ecfc",
+    "url": "/WBSite/static/js/main.9cb01c84.chunk.js"
+  },
+  {
+    "revision": "bbac2b4e523214016c4f",
+    "url": "/WBSite/static/js/runtime-main.dffd35b1.js"
+  }
+]);
